b92a3db3e07ddc5fa2cb3220bc2055fdf0b217b1
196Ikuchil <196thinline@gmail.com>
2020-02-29 13:05:14 +0900

    feat(cpu):Implement LDA routing

--- src/nes/cpu/mod.rs
+++ src/nes/cpu/mod.rs
@@ -20,6 +20,65 @@ pub fn run<T: CpuRegister, U: CpuBus>(register: &mut T, cpu_bus: &mut U) {
 
   match code.name {
     Instruction::LDA if code.mode == Addressing::Immediate => lda_imm(operand, register),
+    Instruction::LDA => lda(operand, register, cpu_bus),
     _ => panic!("Invalid code"),
+  }
+}
+
+#[cfg(test)]
+mod test {
+  use super::super::cpu_register::Register;
+  use super::*;
+
+  struct MockBus {
+    pub memory: Vec<Data>,
+  }
+
+  impl MockBus {
+    fn new() -> Self {
+      MockBus {
+        memory: vec!(0; 65535)
+      }
+    }
+  }
+
+  impl CpuBus for MockBus {
+    fn read(&mut self, a: Addr) -> Data {
+      self.memory[a as usize]
+    }
+
+    fn read_word(&mut self, a: Addr) -> Word {
+      let top = self.read(a) as u16;
+      let low = self.read(a + 1) as u16;
+      ( top << 8 | low ) as Word
+    }
+
+    fn write(&mut self, a: Addr, d: Data)  {
+      self.memory[a as usize] = d
+    }
+  }
+
+  #[test]
+  fn test_run_lda_imm() {
+    let mut b = MockBus::new();
+    let mut r = Register::new();
+    r.set_PC(0x80);
+    b.memory[0x80] = 0xA9;
+    b.memory[0x81] = 0xFF;
+    run(&mut r ,&mut b);
+    assert_eq!(r.get_A(), 0xFF)
+  }
+
+  #[test]
+  fn test_run_lda_zpg_x() {
+    let mut b = MockBus::new();
+    let mut r = Register::new();
+    r.set_PC(0x80);
+    r.set_X(0x01);
+    b.memory[0x80] = 0xB5;
+    b.memory[0x81] = 0x11;
+    b.memory[0x12] = 0xFF;
+    run(&mut r, &mut b);
+    assert_eq!(r.get_A(), 0xFF)
   }
 }
