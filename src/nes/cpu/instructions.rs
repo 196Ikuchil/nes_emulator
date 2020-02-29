0e4c147f15b0dea115758c321a4fe47d836bad1a
196Ikuchil <196thinline@gmail.com>
2020-02-29 13:02:35 +0900

    feat(instructions.rs):Add lda instruction and test

--- src/nes/cpu/instructions.rs
+++ src/nes/cpu/instructions.rs
@@ -11,7 +11,13 @@ pub fn lda_imm<T: CpuRegister>(operand: Word, register: &mut T) {
       .update_status_zero_by(operand as Data);
 }
 
-
+pub fn lda<T: CpuRegister, U: CpuBus>(operand: Word, register: &mut T, bus: &mut U) {
+  let v = bus.read(operand as Addr);
+  register
+    .set_A(v)
+    .update_status_negative_by(v)
+    .update_status_zero_by(v);
+}
 
 
 #[cfg(test)]
@@ -19,10 +25,47 @@ mod test {
   use super::super::super::cpu_register::Register;
   use super::*;
 
+  struct MockBus {
+    pub memory: Vec<Data>,
+  }
+
+  impl MockBus {
+    fn new() -> Self {
+      MockBus {
+        memory: vec!(0; 256)
+      }
+    }
+  }
+
+  impl CpuBus for MockBus {
+    fn read(&mut self, a: Addr) -> Data {
+      self.memory[a as usize]
+    }
+
+    fn read_word(&mut self, a: Addr) -> Word {
+      let top = self.read(a) as u16;
+      let low = self.read(a + 1) as u16;
+      ( top << 8 | low ) as Word
+    }
+
+    fn write(&mut self, a: Addr, d: Data)  {
+      self.memory[a as usize] = d
+    }
+  }
+
   #[test]
   fn test_lda_imm() {
     let mut r = Register::new();
     lda_imm(0xA9, &mut r);
     assert_eq!(r.get_A(), 0xA9);
+  }
+
+  #[test]
+  fn test_lda() {
+    let mut r = Register::new();
+    let mut b = MockBus::new();
+    b.memory[0xA5] = 0xFF;
+    lda(0xA5, &mut r, &mut b);
+    assert_eq!(r.get_A(),0xFF);
   }
 }
