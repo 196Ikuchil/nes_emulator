1989f3c3ba821e26d4dab4bcdd3b3e06b1711383
196Ikuchil <196thinline@gmail.com>
2020-03-08 16:30:19 +0900

    feat(main.rs):Add entrypoint function

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,20 @@ extern crate lazy_static;
 
 mod nes;
 
+use nes::Context;
 
 fn main() {
-    println!("Hello, world!");
+}
+
+#[no_mangle]
+pub fn run(len: usize, ptr: *mut u8) {
+  let buf: &mut [u8] = unsafe{  std::slice::from_raw_parts_mut(ptr, len + 1) };
+  let mut ctx = Context::new(buf);
+  nes::reset(&mut ctx);
+  // externs::cancel_main_loop();
+  let main_loop = || {
+    // let key_state = buf[len -1];
+    nes::run(&mut ctx);
+  };
+  // externs::set_main_loop_callback(main_loop);
 }
