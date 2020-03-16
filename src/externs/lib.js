d3d8fc986e0fd9ed52ad6efe13657bb4085f383e
196Ikuchil <196thinline@gmail.com>
2020-03-17 00:52:25 +0900

    feat(externs/lib.js):Add oscillator methods

--- src/externs/lib.js
+++ src/externs/lib.js
@@ -4,4 +4,22 @@ mergeInto(LibraryManager.library, {
     Module.NES.image.data.set(Module.NES.buf);
     Module.NES.ctx.putImageData(Module.NES.image, 0, 0);
   },
+  start_oscillator: function (index) {
+    Module.NES.oscs[index].start()
+  },
+  stop_oscillator: function (index) {
+    Module.NES.oscs[index].stop()
+  },
+  set_oscillator_frequency: function (index, freq) {
+    Module.NES.oscs[index].setFrequency(freq)
+  },
+  change_oscillator_frequency: function (index, freq) {
+    Module.NES.oscs[index].changeFrequency(freq)
+  },
+  set_oscillator_volume: function (index, volume) {
+    Module.NES.oscs[index].setVolume(volume)
+  },
+  set_oscillator_duty: function (index, duty) {
+    Module.NES.oscs[index].setDuty(duty)
+  }
 });
