9a3564a0276f02f16f1ab2a3dd8cda7fe4ece24e
196Ikuchil <196thinline@gmail.com>
2020-03-24 00:40:53 +0900

    feat(mapper/mod.rs):Implement change by mapper type

---
+++ src/nes/mapper/mod.rs
@@ -0,0 +1,16 @@
+mod mapper0;
+mod mapper;
+
+pub use super::types::*;
+pub use super::rom::Rom;
+pub use self::mapper::Mapper;
+pub use self::mapper0::Mapper0;
+
+impl dyn Mapper {
+  pub fn new(mapper_num: Data) -> Box<dyn Mapper> {
+    match mapper_num {
+      0 => Box::new(Mapper0::new()),
+      _ => Box::new(Mapper0::new()),
+    }
+  }
+}
