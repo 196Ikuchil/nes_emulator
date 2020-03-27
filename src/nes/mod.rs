7b5bbaf3f2e8e02c287f6b6eb84f967cf6f9754a
196Ikuchil <196thinline@gmail.com>
2020-03-27 23:09:07 +0900

    refactor(nes.mod.rs):Call ppu by step

--- src/nes/mod.rs
+++ src/nes/mod.rs
@@ -75,7 +75,11 @@ pub fn run(ctx: &mut Context, key_state: Data){
       cpu::run(&mut ctx.cpu_register, &mut cpu_bus, &mut ctx.nmi) as Word
     };
     ctx.apu.run(cycle);
-    let is_ready = ctx.ppu.run((cycle * 3) as usize, &mut ctx.nmi, &*ctx.mapper);
+    let mut is_ready = false;
+    for _ in 0..cycle*3 { // refactor: step for mapper
+      is_ready |= ctx.ppu.run(1 as usize, &mut ctx.nmi, &*ctx.mapper);
+    }
+
     if is_ready {
       if ctx.ppu.background.0.len() != 0 {
         ctx.renderer.render(&ctx.ppu.background.0, &ctx.ppu.sprites);
